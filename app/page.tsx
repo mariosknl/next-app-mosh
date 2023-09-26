4c02a00942d0b6d4bc0d4bc438510146108dbad0
Marios Knl <mariosknl@outlook.com>
2023-09-26 08:47:36 +0300

    optimizations

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,23 @@ import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
+import coffee from "@/public/images/coffee.jpeg";
+import { Metadata } from "next";
+
 export default async function Home() {
 	const session = await getServerSession(authOptions);
 
 	return (
-		<main>
-			<h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-			<Link href="/users">Users</Link>
-			<ProductCard />
+		<main className="relative h-screen">
+			<Image
+				src="https://bit.ly/react-cover"
+				alt="coffee"
+				fill
+				className="object-cover"
+				sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+				quality={100}
+				priority
+			/>
 		</main>
 	);
 }
